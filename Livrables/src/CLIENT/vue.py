0c96c0ce65f6837dc17df088060f94361aad14d4
Dany Viens <dany.viens@gmail.com>
2021-03-08 17:08:30 -0500

    Insertion dans les nouvelles tables de la BD

--- Livrables/src/CLIENT/vue.py
+++ Livrables/src/CLIENT/vue.py
@@ -51,7 +51,7 @@ class Vue():
         return self.cadrelogin
     
     def creercadreprincipal(self,usager):
-        self.root.title("GestionMedia")
+        self.root.title("Production CDJ")
         self.cadreprincipal=Frame(self.cadreapp,width=400,height=400)
         
         self.cadretitre=Frame(self.cadreapp,width=400,height=400)
